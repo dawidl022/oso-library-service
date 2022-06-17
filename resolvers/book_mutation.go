8b938f049cb782477fa3c1fbf2a600e1f8877fe2
Dawid Lachowicz <domdaw7@gmail.com>
2022-06-17 10:25:03 +0100

    Refactor resolver and add remaining basic mutations

--- resolvers/book_mutation.go
+++ resolvers/book_mutation.go
@@ -16,7 +16,7 @@ type BookMutation struct {
 
 func (b *BookMutation) getUser(userId graphql.ID) models.User {
 	return models.User{
-		Role: "reader",
+		Role: "librarian",
 	}
 }
 
@@ -24,18 +24,38 @@ func (b *BookMutation) getBook(userId graphql.ID) models.Book {
 	return models.Book{}
 }
 
-type readBookArgs struct {
-	ReaderId graphql.ID
-	BookId   graphql.ID
+func (b *BookMutation) getUserAndBookModels(args bookMutationArgs) (*models.User, *models.Book) {
+	user := b.getUser(args.UserId)
+	book := b.getBook(args.BookId)
+	return &user, &book
 }
 
-func (b *BookMutation) ReadBook(args readBookArgs) int32 {
-	user := b.getUser(args.ReaderId)
-	book := b.getBook(args.BookId)
-
-	err := b.oso.Authorize(user, "read", book)
+func (b *BookMutation) getAuthzHttpStatusCode(args bookMutationArgs, permission string) int32 {
+	user, book := b.getUserAndBookModels(args)
+	err := b.oso.Authorize(user, permission, book)
 	if err != nil {
 		return http.StatusForbidden
 	}
 	return http.StatusOK
+}
+
+type bookMutationArgs struct {
+	UserId graphql.ID
+	BookId graphql.ID
+}
+
+func (b *BookMutation) ReadBook(args bookMutationArgs) int32 {
+	return b.getAuthzHttpStatusCode(args, "read")
+}
+
+func (b *BookMutation) CheckoutBook(args bookMutationArgs) int32 {
+	return b.getAuthzHttpStatusCode(args, "checkout")
+}
+
+func (b *BookMutation) CheckinBook(args bookMutationArgs) int32 {
+	return b.getAuthzHttpStatusCode(args, "checkin")
+}
+
+func (b *BookMutation) RemoveBook(args bookMutationArgs) int32 {
+	return b.getAuthzHttpStatusCode(args, "remove")
 }
